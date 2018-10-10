69bff33ab7843e7fc19dcf1e112d58aaef623c81
likendero <likendero11@gmail.com>
2018-10-10 12:45:28 +0200

    spineeerrrrrr

--- Calculadora_Gonzalez_Rives_Javier/app/src/main/java/org/iesalandalus/linuxdiurno/calculadora_gonzalez_rives_javier/MainActivity.java
+++ Calculadora_Gonzalez_Rives_Javier/app/src/main/java/org/iesalandalus/linuxdiurno/calculadora_gonzalez_rives_javier/MainActivity.java
@@ -1,11 +1,16 @@
 package org.iesalandalus.linuxdiurno.calculadora_gonzalez_rives_javier;
 
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtNumero1,txtNumero2;
     private TextView txtRes;
     private RadioButton radioEsp,radioGer,radioJap,radioKur;
+    private Spinner spOpciones;
     //private Button btnSumar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +35,20 @@ public class MainActivity extends AppCompatActivity {
         radioGer = (RadioButton) findViewById(R.id.rgGer);
         radioJap = (RadioButton) findViewById(R.id.rdJapones);
         radioKur = (RadioButton) findViewById(R.id.rdKurdo);
+
+        // captura de espinner
+        spOpciones = (Spinner) findViewById(R.id.spOpciones);
+        // array con las opciones
+        String[] opciones = {"sumar","restar","multiplicar","dividir"};
+        // adapter que transforma el array a objeto
+        ArrayAdapter<String> adap = new ArrayAdapter<String>(this,R.layout.spinner_opciones,opciones);
+        spOpciones.setAdapter(adap);
+
     }
 
     /**
      * metodo que realizara la funcion de sumar
-     * @param view
+     *
      */
     public void sumar(View view){
         int num1,num2;
@@ -51,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * metodo que resta dos numeros
-     * @param view
+     *
      */
     public void restar(View view){
 
@@ -66,7 +81,18 @@ public class MainActivity extends AppCompatActivity {
 
         txtRes.setText( R.string.resutadoEsp + (num1-num2));
     }
-
+    public void dividir(View view){
+        int num1,num2;
+        String valor1,valor2;
+        // guardado de la informacion
+        valor1 = txtNumero1.getText().toString();
+        valor2 = txtNumero2.getText().toString();
+        // paso de cadena a enteros
+        num1 = Integer.parseInt(valor1);
+        num2 = Integer.parseInt(valor2);
+        // seteo resultado
+        txtRes.setText(R.string.resutadoEsp + " " +(num1/num2));
+    }
     /**
      * metodo que define el idioma de la aplicacion en castellano
      * @param view
@@ -110,7 +136,31 @@ public class MainActivity extends AppCompatActivity {
             btnSumar.setText(R.string.sumarKur);
             btnRestar.setText(R.string.restarKur);
         }
+    }
+    public void operacion(View view){
+        String seleccion =  spOpciones.getSelectedItem().toString();
+        String num1,num2;
+        num1 = txtNumero1.getText().toString().trim();
+        num2 = txtNumero2.getText().toString().trim();
+        // control para que halla numeros
+        if(num1.length() > 0 && num2.length() > 0) {
+            switch (seleccion) {
+                case "sumar":
+                    sumar(view);
+                    break;
+                case "restar":
+                    restar(view);
+                    break;
 
+            }
+        }
 
     }
+   /* Spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener){
+        public onContextItemSelected(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+
+        }
+
+    }
+    */
 }
