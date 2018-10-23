e3ce7a42aa4adea93df55137f305a5c5baf26cea
likendero <likendero11@gmail.com>
2018-10-23 14:37:24 +0200

    cosas

--- Peluqueria_Javier_Gonzalez_Rives/app/src/main/java/org/iesalandalus/likendero/peluqueria_javier_gonzalez_rives/AcDatos.java
+++ Peluqueria_Javier_Gonzalez_Rives/app/src/main/java/org/iesalandalus/likendero/peluqueria_javier_gonzalez_rives/AcDatos.java
@@ -2,9 +2,16 @@ package org.iesalandalus.likendero.peluqueria_javier_gonzalez_rives;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class AcDatos extends AppCompatActivity {
-
+    private RadioButton rdPelo[];
+    private ListView lvDias;
+    private String diasSemana[] = {"lunes","martes","miercoles","jueves","viernes","sabado","domingo"};
+    private EditText edNumero,edComentarios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
