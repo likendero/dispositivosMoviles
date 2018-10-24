eda71a5e6d4d293936ff14c5849ef91224a9687f
likendero <likendero11@gmail.com>
2018-10-24 18:52:35 +0200

    cosas

--- Peluqueria_Javier_Gonzalez_Rives/app/src/main/java/org/iesalandalus/likendero/peluqueria_javier_gonzalez_rives/AcDatos.java
+++ Peluqueria_Javier_Gonzalez_Rives/app/src/main/java/org/iesalandalus/likendero/peluqueria_javier_gonzalez_rives/AcDatos.java
@@ -2,12 +2,21 @@ package org.iesalandalus.likendero.peluqueria_javier_gonzalez_rives;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
 
 public class AcDatos extends AppCompatActivity {
-
+    private EditText edCometarios,edTelefono;
+    private ListView lvDias;
+    private RadioButton rdPelo[] = new RadioButton[5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ac_datos);
+
+    }
+    private void registro(){
+
     }
 }
