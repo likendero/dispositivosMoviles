9331ae13df59ac49dd6aa1bbd263b540aeb246f6
likendero <likendero11@gmail.com>
2018-10-31 14:35:02 +0100

    copia y base de datos

---
+++ P11bRepasoSQlite_Javier_Gonzalez_Rives /app/src/main/java/org/iesalandalus/likendero/p11sqlite_javier_gonzalez_rives/AdminSQLiteOpenHelper.java
@@ -0,0 +1,37 @@
+package org.iesalandalus.likendero.p11sqlite_javier_gonzalez_rives;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+/**
+ * @author Javier Gonzalez Rives
+ *
+ */
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    /**
+     * metodo constructor
+     * @param context
+     * @param name
+     * @param factory
+     * @param version
+     */
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL("create table articulos(" +
+                "codigo int primary key," +
+                "descripcion text," +
+                "precio real" +
+                ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int i, int i1) {
+
+    }
+}
