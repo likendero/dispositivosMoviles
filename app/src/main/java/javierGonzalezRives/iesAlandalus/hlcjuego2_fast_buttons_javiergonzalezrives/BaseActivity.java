55dfadd78d451f52aab27e2e076f7b4eaf26cf9c
likendero <likendero11@gmail.com>
2019-01-31 08:26:26 +0100

    cosas disp moviles

---
+++ app/src/main/java/javierGonzalezRives/iesAlandalus/hlcjuego2_fast_buttons_javiergonzalezrives/BaseActivity.java
@@ -0,0 +1,4 @@
+package javierGonzalezRives.iesAlandalus.hlcjuego2_fast_buttons_javiergonzalezrives;
+
+public class BaseActivity {
+}
---
+++ app/src/main/java/javierGonzalezRives/iesAlandalus/hlcjuego2_fast_buttons_javiergonzalezrives/MainActivity.java
@@ -0,0 +1,13 @@
+package javierGonzalezRives.iesAlandalus.hlcjuego2_fast_buttons_javiergonzalezrives;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
