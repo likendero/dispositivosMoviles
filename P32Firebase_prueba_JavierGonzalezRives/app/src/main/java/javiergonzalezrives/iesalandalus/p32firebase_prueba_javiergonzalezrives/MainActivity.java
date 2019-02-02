dfaaec32c4e33eb05b7eb84d80f71e4a8dab8bd8
likendero <likendero11@gmail.com>
2019-02-02 11:47:40 +0100

    asdfsdf

---
+++ P32Firebase_prueba_JavierGonzalezRives/app/src/main/java/javiergonzalezrives/iesalandalus/p32firebase_prueba_javiergonzalezrives/MainActivity.java
@@ -0,0 +1,13 @@
+package javiergonzalezrives.iesalandalus.p32firebase_prueba_javiergonzalezrives;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- PracticaJuegoandroid1/app/src/main/java/javiergonzalezrives/iesalandalus/ejercicio/MainActivity.java
+++ PracticaJuegoandroid1/app/src/main/java/javiergonzalezrives/iesalandalus/ejercicio/MainActivity.java
@@ -2,13 +2,35 @@ package javiergonzalezrives.iesalandalus.ejercicio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spSelector;
+    private final String[] = {
+            "nivel 1",
+            "nivel 2",
+            "nivel 3",
+            "nivel 4",
+            "nivel 5",
+            "nivel 6"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // instanciacion del spinner
+        spSelector = (Spinner) findViewById(R.id.spNivel);
+
+    }
+
+    /**
+     * preparar el spinner para la seleccion del
+     * nivel
+     */
+    public void generarSpinner(){
+
+        ArrayAdapter<String> adapter() = ArrayAdapter.createFromResource(this, android.R.layout.simple_spinner_item,);
 
     }
 }
